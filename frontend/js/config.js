78f333f217ab834c42723b322504cf26abac8766
davit jibladze <38498446+jibladzedato@users.noreply.github.com>
2026-09-09 18:36:47 +0400

    Update config.js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -13,7 +13,7 @@
 //   const API_OVERRIDE = "https://edunity-api.onrender.com/api";
 
 (function () {
-  const API_OVERRIDE = null; // ← заполнить только для продакшна
+  const API_OVERRIDE = https://edunity-ld72.onrender.com/api; // ← заполнить только для продакшна
   const API_PORT = 4000;
 
   let base;
