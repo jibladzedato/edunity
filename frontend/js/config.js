22c5e0d871602dd0cfa265b02bd8ede85d8a0c60
davit jibladze <38498446+jibladzedato@users.noreply.github.com>
2026-09-12 04:11:42 +0400

    Update config.js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -15,9 +15,7 @@
 (function () {
 <<<<<<< HEAD
   const API_OVERRIDE = "https://edunity-ld72.onrender.com/api";// ← заполнить только для продакшна
-=======
-  const API_OVERRIDE = "https://edunity-ld72.onrender.com/api"; // ← заполнить только для продакшна
->>>>>>> fa70e83 (nosmtp)
+
   const API_PORT = 4000;
 
   let base;
